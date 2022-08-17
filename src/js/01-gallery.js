90c1e69b2cf87db413b9fe02e916ff05d1a7c571
Roman-Nechaev <nrchaevroman@gmail.com>
2022-08-17 15:08:07 +0300

    tack1-ok

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+/////////////////////////////////////
+const galleryRef = document.querySelector('.gallery');
+
+const rendererImage = MarkupRendererImage(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', rendererImage);
+
+function MarkupRendererImage(gallery) {
+  return gallery
+
+    .map(({ preview, original, description }) => {
+      return `
+  <a class="gallery__item" href="${original}">
+
+  <img class="gallery__image" src="${preview}" 
+
+  alt="${description}" />
+</a>`;
+    })
+    .join('');
+} // рендер  разметки гелереи
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionDelay: 250,
+}); // библиотека
